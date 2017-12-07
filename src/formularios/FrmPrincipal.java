6d047d23ad75fe1d0914064399f9ba3f6e3f93de
Ing Arnaldo Castilla <arnaldo.castilla@hotmail.com>
2017-12-07 00:04:01 -0500

    Menu de navegacion formulario principal, con imagenes

--- src/formularios/FrmPrincipal.java
+++ src/formularios/FrmPrincipal.java
@@ -27,8 +27,84 @@ public class FrmPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jMenuBar1 = new javax.swing.JMenuBar();
+        mnuArchivo = new javax.swing.JMenu();
+        mnuArchivoClientes = new javax.swing.JMenuItem();
+        mnuArchivoProductos = new javax.swing.JMenuItem();
+        mnuArchivoUsuarios = new javax.swing.JMenuItem();
+        jSeparator1 = new javax.swing.JPopupMenu.Separator();
+        mnuArchivoCambioClave = new javax.swing.JMenuItem();
+        mnuArchivoCambioUsuario = new javax.swing.JMenuItem();
+        jSeparator2 = new javax.swing.JPopupMenu.Separator();
+        mnuArchivoSalir = new javax.swing.JMenuItem();
+        mnuMovimientos = new javax.swing.JMenu();
+        mnuMovimientosNuevaFactura = new javax.swing.JMenuItem();
+        mnuMovimientosReporteFactura = new javax.swing.JMenuItem();
+        mnuAyuda = new javax.swing.JMenu();
+        mnuAyudaAcercaDe = new javax.swing.JMenuItem();
+        mnuAyudaAyuda = new javax.swing.JMenuItem();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Sistema de facturación");
+
+        mnuArchivo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/archivo.png"))); // NOI18N
+        mnuArchivo.setText("Archivo");
+
+        mnuArchivoClientes.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/cliente.png"))); // NOI18N
+        mnuArchivoClientes.setText("Clientes");
+        mnuArchivo.add(mnuArchivoClientes);
+
+        mnuArchivoProductos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/productos.png"))); // NOI18N
+        mnuArchivoProductos.setText("Productos");
+        mnuArchivo.add(mnuArchivoProductos);
+
+        mnuArchivoUsuarios.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/usuarios.png"))); // NOI18N
+        mnuArchivoUsuarios.setText("Usuarios");
+        mnuArchivo.add(mnuArchivoUsuarios);
+        mnuArchivo.add(jSeparator1);
+
+        mnuArchivoCambioClave.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/cambio_clave.png"))); // NOI18N
+        mnuArchivoCambioClave.setText("Cambio Clave");
+        mnuArchivo.add(mnuArchivoCambioClave);
+
+        mnuArchivoCambioUsuario.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/cambio_user.png"))); // NOI18N
+        mnuArchivoCambioUsuario.setText("Cambio Usuario");
+        mnuArchivo.add(mnuArchivoCambioUsuario);
+        mnuArchivo.add(jSeparator2);
+
+        mnuArchivoSalir.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/salir.png"))); // NOI18N
+        mnuArchivoSalir.setText("Salir");
+        mnuArchivo.add(mnuArchivoSalir);
+
+        jMenuBar1.add(mnuArchivo);
+
+        mnuMovimientos.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/movimientos.png"))); // NOI18N
+        mnuMovimientos.setText("Movimientos");
+
+        mnuMovimientosNuevaFactura.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/factura.png"))); // NOI18N
+        mnuMovimientosNuevaFactura.setText("Nueva Factura");
+        mnuMovimientos.add(mnuMovimientosNuevaFactura);
+
+        mnuMovimientosReporteFactura.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/nueva factura.png"))); // NOI18N
+        mnuMovimientosReporteFactura.setText("Reporte Factura");
+        mnuMovimientos.add(mnuMovimientosReporteFactura);
+
+        jMenuBar1.add(mnuMovimientos);
+
+        mnuAyuda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/help.png"))); // NOI18N
+        mnuAyuda.setText("Ayuda");
+
+        mnuAyudaAcercaDe.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/acerca_de.png"))); // NOI18N
+        mnuAyudaAcercaDe.setText("Acerca de");
+        mnuAyuda.add(mnuAyudaAcercaDe);
+
+        mnuAyudaAyuda.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/ayuda.png"))); // NOI18N
+        mnuAyudaAyuda.setText("Ayuda");
+        mnuAyuda.add(mnuAyudaAyuda);
+
+        jMenuBar1.add(mnuAyuda);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -38,7 +114,7 @@ public class FrmPrincipal extends javax.swing.JFrame {
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 245, Short.MAX_VALUE)
         );
 
         pack();
@@ -80,5 +156,21 @@ public class FrmPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JPopupMenu.Separator jSeparator1;
+    private javax.swing.JPopupMenu.Separator jSeparator2;
+    private javax.swing.JMenu mnuArchivo;
+    private javax.swing.JMenuItem mnuArchivoCambioClave;
+    private javax.swing.JMenuItem mnuArchivoCambioUsuario;
+    private javax.swing.JMenuItem mnuArchivoClientes;
+    private javax.swing.JMenuItem mnuArchivoProductos;
+    private javax.swing.JMenuItem mnuArchivoSalir;
+    private javax.swing.JMenuItem mnuArchivoUsuarios;
+    private javax.swing.JMenu mnuAyuda;
+    private javax.swing.JMenuItem mnuAyudaAcercaDe;
+    private javax.swing.JMenuItem mnuAyudaAyuda;
+    private javax.swing.JMenu mnuMovimientos;
+    private javax.swing.JMenuItem mnuMovimientosNuevaFactura;
+    private javax.swing.JMenuItem mnuMovimientosReporteFactura;
     // End of variables declaration//GEN-END:variables
 }
