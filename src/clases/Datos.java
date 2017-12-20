c42970aed449d11ebaeb8ddf10986aca1cbcbc27
Ing Arnaldo Castilla <arnaldo.castilla@hotmail.com>
2017-12-19 23:28:50 -0500

    Cramos un vector de 50 posiciones llamado misUsuarios, una variable auxiliar conUsu, en el constructor de la por defecto de la clase datos, se instancia un objecto de la clase usuario y se pasan los datos quemados, luego modigicamos el metodo validarUsuario para que interactue el inicio de sesion con el vector de usuarios

--- src/clases/Datos.java
+++ src/clases/Datos.java
@@ -3,11 +3,23 @@ package clases;
 
 
 public class Datos {
+    
+    private Usuario misUsuarios[] = new Usuario[50];
+    private int conUsu = 0;
+
+    public Datos() {
+        Usuario miUsuario = new Usuario("rafa", "Arnaldo Rafael", "Castilla", "123", 1);
+        misUsuarios[conUsu] = miUsuario;
+        conUsu++;
+    }
+    
     public boolean validarUsuario(String usuario, String clave){
-        if(usuario.equals("zulu") && clave.equals("123")){
-            return true;
-        }else{
-            return false;
+        boolean aux = false;
+        for(int i=0;i<conUsu;i++){
+            if(misUsuarios[i].getIdUsuario().equals(usuario) && misUsuarios[i].getClave().equals(clave) ){
+                return true;
+            }
         }
+        return false;
     }
 }
