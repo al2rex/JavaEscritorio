b9e3f80fb1cec54ac3e467b9a9f521c2a1cdfc9c
Ing Arnaldo Castilla <arnaldo.castilla@hotmail.com>
2017-12-27 22:44:13 -0500

    pasamos el objeto Datos a la clase principal y lo llamamos en cada formulario!

--- src/clases/JavaEscritorio.java
+++ src/clases/JavaEscritorio.java
@@ -5,7 +5,10 @@ import formularios.FrmLogin;
 public class JavaEscritorio {
     
     public static void main(String[] args) {
+        Datos misDatos = new Datos();
+         
         FrmLogin frmLogin = new FrmLogin();
+        frmLogin.setDatos(misDatos);
         frmLogin.setLocationRelativeTo(null);
         frmLogin.setVisible(true);
     }
